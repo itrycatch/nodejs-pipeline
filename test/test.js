8c7263cae6455d85052d4f6cb7e386eba4684f31
mdas <mdas@localhost.localdomain>
2017-11-29 17:57:07 +0530

    Added the tests

--- test/test.js
+++ test/test.js
@@ -1,28 +1,29 @@
-const chai=require('chai')
-const expect=require('chai').expect;
+const chai = require('chai')
+const expect = require('chai').expect;
 
 chai.use(require('chai-http'));
 
-const app=require('../index.js');
+const app = require('../index.js');
 
-describe('The API endpoint /users',function(){
-    it('Get the user',function(){
+describe('The API endpoint /users', function () {
+
+    it('Get the user', function () {
         return chai.request(app)
-                .get('/users')
-                .then(function(res){
-                    expect(res).to.have.status(200);
-                    expect(res).to.be.string;
-                });
+            .get('/users')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.string;
+            });
     });
 
-    it('should return Not Found', function() {
+    it('should return Not Found', function () {
         return chai.request(app)
-          .get('/INVALID_PATH')
-          .then(function(res) {
-            throw new Error('Path exists!');
-          })
-          .catch(function(err) {
-            expect(err).to.have.status(404);
-          });
-      });
+            .get('/INVALID_PATH')
+            .then(function (res) {
+                throw new Error('Path exists!');
+            })
+            .catch(function (err) {
+                expect(err).to.have.status(404);
+            });
+    });
 });
